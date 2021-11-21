ba6e9c3a06fd661e7e4233da8ce7b044ba897636
adafycheng <adafycheng3110@gmail.com>
2021-11-21 17:41:03 +0000

    1. Add info for GitHub, LinkedIn, Stackoverflow and Twitter. 2. Update UI and content. 3. Update Post URL for Contact Form.

---
+++ precache-manifest.5e65a33e7e8e3b447cf64cbfdf167875.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0e3f07116c9def8c4fc7bc448fc328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ae4ee37ed34c625c89",
+    "url": "/static/css/2.f6982f2f.chunk.css"
+  },
+  {
+    "revision": "cd6cfeac6238bc840f55",
+    "url": "/static/css/main.6c5cbba8.chunk.css"
+  },
+  {
+    "revision": "90ae4ee37ed34c625c89",
+    "url": "/static/js/2.dd43a9b9.chunk.js"
+  },
+  {
+    "revision": "923c0525d335073d7bef4ce81c5cda28",
+    "url": "/static/js/2.dd43a9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d8bd8df72b7446cc07",
+    "url": "/static/js/3.9d919e25.chunk.js"
+  },
+  {
+    "revision": "cd6cfeac6238bc840f55",
+    "url": "/static/js/main.1933d8d5.chunk.js"
+  },
+  {
+    "revision": "bb9a100b4f15fbe8df9f",
+    "url": "/static/js/runtime-main.71530fcb.js"
+  }
+]);

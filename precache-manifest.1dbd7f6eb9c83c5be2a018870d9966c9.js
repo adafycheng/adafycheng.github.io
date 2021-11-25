e7c50deaffc19d48470815798a4c8e587a382e93
adafycheng <adafycheng3110@gmail.com>
2021-11-25 20:35:23 +0000

    1. Add link for Code Pen. 2. Add work experiences.

---
+++ precache-manifest.1dbd7f6eb9c83c5be2a018870d9966c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f52929599ed92d132d649481d67ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "468444ac37467733ca51",
+    "url": "/static/css/2.eaf62b8a.chunk.css"
+  },
+  {
+    "revision": "85325e039d04103ccb39",
+    "url": "/static/css/main.432b23f4.chunk.css"
+  },
+  {
+    "revision": "468444ac37467733ca51",
+    "url": "/static/js/2.23a51423.chunk.js"
+  },
+  {
+    "revision": "923c0525d335073d7bef4ce81c5cda28",
+    "url": "/static/js/2.23a51423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d31604090aacb72cb3d",
+    "url": "/static/js/3.a3c791dc.chunk.js"
+  },
+  {
+    "revision": "85325e039d04103ccb39",
+    "url": "/static/js/main.1cad5538.chunk.js"
+  },
+  {
+    "revision": "55b0e93a8172f98102fd",
+    "url": "/static/js/runtime-main.8bef9081.js"
+  }
+]);
